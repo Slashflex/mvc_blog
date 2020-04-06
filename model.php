73d0f11fe8467ccefa150472b27b49c432ac7a56
David <pro.davidsaoud@gmail.com>
2020-04-06 21:45:43 +0200

    add: models

---
+++ model.php
@@ -0,0 +1,40 @@
+<?php
+require('./config.php');
+
+function getPosts()
+{
+  $db = dbConnect();
+  // On récupère les 5 derniers billets
+  $req = $db->query("SELECT id, title, content, to_char(creation_date, 'dd-mm-YYYY à HH24:MM:SS') AS creation_date_fr FROM posts ORDER BY creation_date DESC");
+
+  return $req;
+}
+
+function getPost($postId)
+{
+  $db = dbConnect();
+  $req = $db->prepare("SELECT id, title, content, to_char(creation_date, 'dd-mm-YYYY à HH24:MM:SS') AS creation_date_fr FROM posts WHERE id = ?");
+  $req->execute(array($postId));
+  $post = $req->fetch();
+
+  return $post;
+}
+
+function getComments($postId)
+{
+  $db = dbConnect();
+  $comments = $db->prepare("SELECT id, author, comment, to_char(creation_date, 'dd-mm-YYYY à HH24:MM:SS') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC");
+  $comments->execute(array($postId));
+
+  return $comments;
+}
+
+function dbConnect()
+{
+  try {
+    $db = new PDO('pgsql:host=localhost;dbname=test', 'david', DB_PASSWORD);
+    return $db;
+  } catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+  }
+}
